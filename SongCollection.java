442039a7f12e4006e23096c53dcfb9e28f45a089
xtr3mscorch <40750261+xtr3mscorch@users.noreply.github.com>
2020-04-28 12:50:25 +1000

    SongCollection setup
    basic options, exit program..
    no linking / storing yet.

--- SongCollection.java
+++ SongCollection.java
@@ -1,4 +1,4 @@
-
+import java.util.*;
 /**
  * Write a description of class Album here.
  *
@@ -7,13 +7,43 @@
  */
 public class SongCollection
 {
-	private Album album1, album2, album3;
+	//private Album album1, album2, album3;
+	
+	int action1;
 
-	public void run(){
-		//this method should control the flow of the program
+	public void run()
+	{
+		Scanner console = new Scanner(System.in);
+			
+		System.out.print("Please choose an action \n");
+		
+		System.out.print("1 - View Albums: \n");	
+		System.out.print("2 - Song search: \n");
+		System.out.print("3 - Exit program: \n");
+
+		
+		action1 = console.nextInt();	
+        switch (action1)		
+		{
+			case 1:
+			System.out.print("You have no albums!");
+			run();
+			case 2:
+			System.out.print("You have no songs!");
+			run();
+			case 3:
+			System.exit(0);
+			default:
+			System.out.print("Fatal Error... Re booting \n");
+			run();
+		}
+		
+		
 	}
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		SongCollection sg = new SongCollection();
+		System.out.print("Welcome to spoofify \n");
 		sg.run();
 	}
 }
