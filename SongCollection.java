ae3ae482a0cea02a1238c714d553976b5d3136a8
xtr3mscorch <40750261+xtr3mscorch@users.noreply.github.com>
2020-04-28 18:53:16 +1000

    Removed Previous Changes

--- SongCollection.java
+++ SongCollection.java
@@ -8,8 +8,8 @@ Version: 28/04/2020
 //////////////////////////////////////////////////////////////////////////////////////////////
 public class SongCollection
 {
-	private Album album1, album2, album3;
-	int action1;
+	//private Album album1, album2, album3;
+	//private Album album1;
 	Scanner console = new Scanner(System.in);
 
 	//////////////////////////////////////////////////////////////////////////////////////////////
@@ -20,53 +20,37 @@ public class SongCollection
 			+ "1: New album\n"
 			+ "2: View albums\n"
 			+ "3: Song search\n"
-			+ "4: Exit program\n"
-		    + "5: Add Song (Testing)\n"
-			+ "6: View Song (Testing)\n");
-
+			+ "4: Exit program\n");
 
 		System.out.print(mainMenu);
-		action1 = console.nextInt();
+		int action1 = console.nextInt();
 		console.nextLine(); // without this, console will skip any NextLine() input.. weird
         switch (action1)		
 		{
 			case 1:
 				//Here we will create an album
-				System.out.print("Here we will create an album");
+				System.out.print("Here we will create an album\n");
 			case 2:
 				//Here we will view all albums
-				System.out.print("Here we will view all album");
+				System.out.print("Here we will view all album\n");
 			case 3:
 				//Here we will search songs
-				System.out.print("You have no songs!");
+				System.out.print("You have no songs!\n");
 				run();
 			case 4:
 				//Here we will exit program
 				System.exit(0);
-			case 5:
-				//testing...
-				String name;
-				Song s = new Song();
-				System.out.print("Please enter song name: \n");
-				name = console.nextLine();
-				s.setName(name);
-				System.out.print("Song entered successfully \n");
-				run();
-			case 6:
-				
 			default:
 				System.out.print("Fatal Error... Re booting \n");
 			    run();
 		}
 		System.out.print("End \n");
-		
-		
 	}
 	//////////////////////////////////////////////////////////////////////////////////////////////
 	public static void main(String[] args) 
 	{
 		SongCollection sg = new SongCollection();
-		System.out.print("Welcome to spoofify \n");
+		System.out.print("Welcome to spotify \n");
 		sg.run();
 	}
 	//////////////////////////////////////////////////////////////////////////////////////////////
